f9c139d2f756ea1d8cff7559f151d6b7b13c1785
khalidhersi <cipherpolfoundation@gmail.com>
2021-11-22 11:52:29 +0000

    tests created

---
+++ main.test.js
@@ -0,0 +1,137 @@
+import { it, expect } from "@jest/globals";
+import { translate } from "./main";
+
+/* Alphabet translation */
+
+//Translates a to 
+it("Translates a to " , () => {
+    const result = translate()
+    expect(result).toBe()
+})
+//Translates b to 
+it("Translates b to " , () => {
+    const result = translate()
+    expect(result).toBe()
+})
+//Translates c to 
+it("Translates c to " , () => {
+    const result = translate()
+    expect(result).toBe()
+})
+//Translates d to 
+it("Translates d to " , () => {
+    const result = translate()
+    expect(result).toBe()
+})
+//Translates e to 
+it("Translates e to " , () => {
+    const result = translate()
+    expect(result).toBe()
+})
+//Translates f to 
+it("Translates f to " , () => {
+    const result = translate()
+    expect(result).toBe()
+})
+//Translates g to 
+it("Translates g to " , () => {
+    const result = translate()
+    expect(result).toBe()
+})
+//Translates h to 
+it("Translates h to " , () => {
+    const result = translate()
+    expect(result).toBe()
+})
+//Translates i to 
+it("Translates i to " , () => {
+    const result = translate()
+    expect(result).toBe()
+})
+//Translates j to 
+it("Translates j to " , () => {
+    const result = translate()
+    expect(result).toBe()
+})
+//Translates k to 
+it("Translates k to " , () => {
+    const result = translate()
+    expect(result).toBe()
+})
+//Translates l to 
+it("Translates l to " , () => {
+    const result = translate()
+    expect(result).toBe()
+})
+//Translates m to 
+it("Translates m to " , () => {
+    const result = translate()
+    expect(result).toBe()
+})
+//Translates n to 
+it("Translates n to " , () => {
+    const result = translate()
+    expect(result).toBe()
+})
+//Translates o to 
+it("Translates o to " , () => {
+    const result = translate()
+    expect(result).toBe()
+})
+//Translates p to 
+it("Translates p to " , () => {
+    const result = translate()
+    expect(result).toBe()
+})
+//Translates q to 
+it("Translates q to " , () => {
+    const result = translate()
+    expect(result).toBe()
+})
+//Translates r to 
+it("Translates r to " , () => {
+    const result = translate()
+    expect(result).toBe()
+})
+//Translates s to 
+it("Translates s to " , () => {
+    const result = translate()
+    expect(result).toBe()
+})
+//Translates t to 
+it("Translates t to " , () => {
+    const result = translate()
+    expect(result).toBe()
+})
+//Translates u to 
+it("Translates u to " , () => {
+    const result = translate()
+    expect(result).toBe()
+})
+//Translates v to 
+it("Translates v to " , () => {
+    const result = translate()
+    expect(result).toBe()
+})
+//Translates w to 
+it("Translates w to " , () => {
+    const result = translate()
+    expect(result).toBe()
+})
+//Translates x to 
+it("Translates x to " , () => {
+    const result = translate()
+    expect(result).toBe()
+})
+//Translates y to 
+it("Translates y to " , () => {
+    const result = translate()
+    expect(result).toBe()
+})
+//Translates z to 
+it("Translates z to " , () => {
+    const result = translate()
+    expect(result).toBe()
+})
+
+
